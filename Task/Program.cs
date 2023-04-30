6e89afe8c81a71fb3a74d576c9b940770cac688a
Alex <sasha-boriskov@yandex.ru>
2023-04-30 19:08:43 +0300

    Вывод нового массива с отсорт. строками

--- Task/Program.cs
+++ Task/Program.cs
@@ -22,3 +22,21 @@ string [] array = new string[N];
 NewArray(array);
 Console.WriteLine();
 Console.WriteLine ("["+string.Join(", ", array)+"]");
+Console.WriteLine();
+
+Console.Write ("Введите количество символов для сортировки: ");
+int G = Convert.ToInt32(Console.ReadLine());
+
+string [] newArray = new string[0];
+int count = 0;
+for (int i = 0; i<array.Length; i++)
+{
+    if (array[i].Length <= G) 
+    {
+        count++;
+        Array.Resize(ref newArray, count);
+        newArray[count-1] = array[i];
+    }  
+}
+
+Console.WriteLine ("["+string.Join(", ", newArray)+"]");
