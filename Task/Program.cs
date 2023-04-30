26090e470c35669e999568f285897fa087bd1fb6
Alex <sasha-boriskov@yandex.ru>
2023-04-30 16:27:55 +0300

    Создание массива и заполнение

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк
+// формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам.
+
+void NewArray (string [] array)
+{
+    for (int i=0; i<array.Length;i++)
+    {
+        do 
+        {
+            Console.Write("Введите элемент №"+(i+1)+": ");
+            array[i] = Console.ReadLine();
+        } while (array[i] == "");
+    }
+}
+
+Console.Clear();
+Console.Write ("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string [] array = new string[N];
+NewArray(array);
+Console.WriteLine();
+Console.WriteLine ("["+string.Join(", ", array)+"]");
